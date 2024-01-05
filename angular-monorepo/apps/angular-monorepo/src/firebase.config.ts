643da89f3e5bb8192dd498800d0df2d6eaee36c5
Adrian Dąbek <adrian950113@gmail.com>
2024-01-05 10:59:03 +0100

    add firebase

---
+++ angular-monorepo/apps/angular-monorepo/src/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAb_kAPmTsh-9qiDOPGYT5Zn5QjMI6RjDU",
+  authDomain: "bombos-1f119.firebaseapp.com",
+  projectId: "bombos-1f119",
+  storageBucket: "bombos-1f119.appspot.com",
+  messagingSenderId: "254880634541",
+  appId: "1:254880634541:web:6e0a9a38b942215dbbd824"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
