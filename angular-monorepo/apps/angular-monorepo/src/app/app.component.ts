8180ca851672c0b02c1f3ecf25d12f56704e7ec5
Adrian Dąbek <adrian950113@gmail.com>
2024-01-05 13:30:36 +0100

    add firestore

--- angular-monorepo/apps/angular-monorepo/src/app/app.component.ts
+++ angular-monorepo/apps/angular-monorepo/src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {AsyncPipe, JsonPipe} from "@angular/common";
+import {Component, inject} from '@angular/core';
+import {collection, collectionData, Firestore} from "@angular/fire/firestore";
+import {RouterModule} from '@angular/router';
+import {FirebaseModule} from "../firebase.module";
+import {NxWelcomeComponent} from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    FirebaseModule,
+    AsyncPipe,
+    JsonPipe,
+  ],
   selector: 'angular-monorepo-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-monorepo';
+  firestore: Firestore = inject(Firestore);
+  itemCollection = collection(this.firestore, 'test');
+  item$ = collectionData(this.itemCollection);
 }
