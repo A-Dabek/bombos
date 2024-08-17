2103867d00dbe9daccfb9e072af0dd4b2e53ddcd
Adrian Dabek <adrian950113@gmail.com>
2024-08-17 17:29:41 +0200

    migrate to fire @18

--- apps/bombos/src/app/app.config.ts
+++ apps/bombos/src/app/app.config.ts
@@ -10,21 +10,18 @@ import { appRoutes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withComponentInputBinding()),
-    importProvidersFrom([
-      BrowserModule,
-      BrowserAnimationsModule,
-      provideFirebaseApp(() =>
-        initializeApp({
-          apiKey: 'AIzaSyAb_kAPmTsh-9qiDOPGYT5Zn5QjMI6RjDU',
-          authDomain: 'bombos-1f119.firebaseapp.com',
-          projectId: 'bombos-1f119',
-          storageBucket: 'bombos-1f119.appspot.com',
-          messagingSenderId: '254880634541',
-          appId: '1:254880634541:web:6e0a9a38b942215dbbd824',
-        })
-      ),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-    ]),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyAb_kAPmTsh-9qiDOPGYT5Zn5QjMI6RjDU',
+        authDomain: 'bombos-1f119.firebaseapp.com',
+        projectId: 'bombos-1f119',
+        storageBucket: 'bombos-1f119.appspot.com',
+        messagingSenderId: '254880634541',
+        appId: '1:254880634541:web:6e0a9a38b942215dbbd824',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    importProvidersFrom([BrowserModule, BrowserAnimationsModule]),
   ],
 };
